a5dabc5e384a77009fe4108eb823bb64adb187c8
Erik <erikmompean@gmail.com>
2017-11-11 21:26:27 +0100

    Tablero done!

--- src/models/Tablero.java
+++ src/models/Tablero.java
@@ -5,10 +5,41 @@
  */
 package models;
 
+import java.util.ArrayList;
+import javax.swing.Timer;
+
 /**
  *
  * @author Manel
  */
 public class Tablero {
     //Erik
+    ArrayList<Casilla> casillas;
+    Timer contador;
+    Jugador jugador1, jugador2;
+
+    public Tablero(ArrayList<Casilla> casillas, Jugador jugador1, Jugador jugador2) {
+        this.casillas = casillas;
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+        // TODO Timer
+    }
+
+    public ArrayList<Casilla> getCasillas() {
+        return casillas;
+    }
+
+    public Timer getContador() {
+        return contador;
+    }
+
+    public Jugador getJugador1() {
+        return jugador1;
+    }
+
+    public Jugador getJugador2() {
+        return jugador2;
+    }
+    
+    
 }
