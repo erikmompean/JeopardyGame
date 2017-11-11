938bfd6628a4ef3501093f21f08f3ac78830243c
Erik <erikmompean@gmail.com>
2017-11-11 21:18:16 +0100

    Revert "Commit de prueba"
    This reverts commit a7653a6c75717d1265dab7284db45e02bacbaca9.

--- src/models/Casilla.java
+++ src/models/Casilla.java
@@ -11,44 +11,7 @@ package models;
  */
 public class Casilla {
     //Erik
-    final int NUMERO_MAXIMO_DE_RESPUESTAS = 4;
-    int puntuacion, id, respuesta;
+    int puntuacion;
     String pregunta;
-    String[] respuestas =  new String[NUMERO_MAXIMO_DE_RESPUESTAS];
-
-    public Casilla( int id, int puntuacion,String pregunta, int respuesta, String respuesta1, String[] respuestas ) {
-        this.puntuacion = puntuacion;
-        this.id = id;
-        this.respuesta = respuesta;
-        this.pregunta = pregunta;
-        if (respuestas.length == NUMERO_MAXIMO_DE_RESPUESTAS){
-            for(int i = 0; i < respuestas.length ;i++){
-                this.respuestas[i] = respuestas[i];
-            }
-        }else{
-            //TODO error system
-        }
-    }
-
-    public int getPuntuacion() {
-        return puntuacion;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public int getRespuesta() {
-        return respuesta;
-    }
-
-    public String getPregunta() {
-        return pregunta;
-    }
-
-    public String[] getRespuestas() {
-        return respuestas;
-    }
-    
-    
+    String[] respuesta =  new String[4];
 }
