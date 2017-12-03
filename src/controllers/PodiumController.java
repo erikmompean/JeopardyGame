ea570fdbde1692bc24f69e252a5d4592b0210a97
Manel Romero Garcia <manel.romero.garcia@gmail.com>
2017-12-03 17:17:23 +0100

    Controller Podio3/12/2017

--- src/controllers/PodiumController.java
+++ src/controllers/PodiumController.java
@@ -5,6 +5,9 @@
  */
 package controllers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import models.Jugador;
 import views.Podium;
 
 /**
@@ -12,7 +15,29 @@ import views.Podium;
  * @author Manel
  */
 public class PodiumController {
+
     //Manel
     private Podium podio;
-    
+    private Jugador jugador1;
+    private Jugador jugador2;
+
+    public PodiumController(Podium podio, Jugador jugador1, Jugador jugador2) {
+        this.podio = podio;
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+        posicionar();
+    }
+
+    private void posicionar() {
+        String text = jugador1.getNickName();
+        String text2 = jugador2.getNickName();
+        if (jugador1.getPuntuacion() > jugador2.getPuntuacion()) {
+            podio.pos1.setText(text);
+            podio.pos2.setText(text2);
+        } else {
+            podio.pos2.setText(text2);
+            podio.pos1.setText(text);
+        }
+    }
+
 }
